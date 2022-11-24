7d3263012aa7765f89d7149eccce7092c9419099
AgredaLuis <luis.agreda98@gmail.com>
2022-11-24 10:04:12 -0400

    primer commits del proyecto para subir

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Sidebar from "./components/sidebar/Sidebar"
+import Services from './components/services/Services'
+import Resume from './components/resume/Resume'
+import Home from './components/home/Home'
+import About from './components/about/About'
+import Portfolio from './components/Portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Blog from './components/blog/Blog'
+import Contact from './components/contact/Contact'
+import Pricing from './components/pricing/Pricing'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <Sidebar />
+        <main className='main'>
+          <Home/>
+          <About />
+          <Services/>
+          <Resume />
+          <Portfolio />
+          <Pricing />
+          <Testimonials />
+          <Blog />
+          <Contact />
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function HeaderSocials() {
+  return (
+    <div className="home__socials">
+        <a href="https://www.instagram.com/" className="home__social-link" target="_blank">
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+
+        <a href="https://www.twitter.com/" className="home__social-link" target="_blank">
+            <i class="fa-brands fa-twitter"></i>
+        </a>
+
+        <a href="https://github.com/AgredaLuis" className="home__social-link" target="_blank">
+            <i class="fa-brands fa-github"></i>
+        </a>
+
+        <a href="https://www.pinterest.com/" className="home__social-link" target="_blank">
+            <i class="fa-brands fa-pinterest"></i>
+        </a>
+    </div>
+  )
+}
+
+export default HeaderSocials
